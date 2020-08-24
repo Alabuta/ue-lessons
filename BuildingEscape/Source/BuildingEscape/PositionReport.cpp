76368b2988c49f471b9ff4766523c40e4a5ee7d5
Almaz Kinziabulatov <auqolaq@gmail.com>
2020-08-24 21:27:57 +0300

    Work in progress.

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -10,8 +10,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -20,8 +18,12 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	auto objectName = GetOwner()->GetName();
+
+	auto &&objectTransform = GetOwner()->GetTransform();
+	auto objectPosition = objectTransform.GetLocation();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position Report: object '%s' is at %s "), *objectName, *objectPosition.ToString())
 }
 
 
